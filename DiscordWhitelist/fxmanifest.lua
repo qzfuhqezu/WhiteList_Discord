b7036904e4894f65e21002fac1b6f95b8287e8c3
WIZOX <75537847+WIZOX@users.noreply.github.com>
2021-04-27 16:25:43 +0200

    Update fxmanifest.lua

--- DiscordWhitelist/fxmanifest.lua
+++ DiscordWhitelist/fxmanifest.lua
@@ -1,12 +1,11 @@
-----------------------------------------
---- Discord Whitelist, Made by FAXES ---
-----------------------------------------
+------------------------
+--- Discord Whitelist --
+------------------------
 
 fx_version 'bodacious'
 game 'gta5'
 server_only 'yes'
 
-author 'FAXES'
-description 'Hate updating those ACE Permission white-lists? Well just use Discord! Now you can thanks to this script, and @IllusiveTeas. So keep white lists easy and breezy. This script checks the connecting players Discord roles and checks whether they have the specified role.'
+description 'Discord Whitelist-FiveM
 
 server_script 'server.lua'
